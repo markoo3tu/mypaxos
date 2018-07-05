e4872a31b994254009dbd2a150df8920c67e34d9
markoo3tu <mjhu@sina.com>
2018-07-06 07:18:28 +0800

    this a test

--- MtPaxosLeader.py
+++ MtPaxosLeader.py
@@ -82,4 +82,121 @@ class MtPaxosLeader:
         self.hbListener.doAbort()
         self.msgPump.doAbort()
         self.stopped = True
-        
+
+    def setPrimary(self, primary):
+        if self.isPrimary != primary:
+            if primary:
+                print(u"leader: {} changes to leader".format(self.port))
+            else:
+                print(u"leader: {} changes to no leader".format(self.port))
+        self.isPrimary = primary
+    
+    def getGroup(self):
+        return self.group
+
+    def getLeaders(self):
+        return self.leaders
+    
+    def getAcceptors(self):
+        return self.acceptors
+    
+    def getQuorumSize(self):
+        return int(len(self.getAcceptors()) / 2) + 1
+
+    def getInstanceValue(self, instanceID):
+        if instanceID in self.instances:
+            return self.instances[instanceID].value
+        return None
+    
+    def getHistory(self):
+        return [self.getInstanceValue(i) for i in range(0, self.highestInstance + 1)]
+
+    def getNumAccepted(self):
+        return len([v for v in self.getHistory() if v != None])
+
+    # ---------------------------------------------------
+    def findAndFillGaps(self):
+        # if no message is received, we take the chance to do a little cleanup
+        for i in range(0, self.highestInstance):
+            if self.getInstanceValue(i) == None:
+                print('fill the None instance: {}'.format(i))
+                self.newProposal(0, i)
+        self.lasttime = time.time()
+    
+    def garbageCollect(self):
+        for i in self.instances:
+            self.instances[i].cleanProtocols()
+
+    def recvMessage(self, message):
+        if self.stopped:
+            return
+        if message == None:
+            if self.isPrimary and time.time() - self.lasttime > 15.0:
+                self.findAndFillGaps() # fill some proposal
+                self.garbageCollect()
+            return
+        if message.command == MtMessage.MSG_HEARTBEAT:
+            self.hbListener.newHB(message)
+            return True
+        if message.command == MtMessage.MSG_EXT_PROPOSE:
+            print(u'leader:{} new request value:{} current instance:{}'.format(self.port, message.value, self.highestInstance))
+            if self.isPrimary:
+                self.newProposal(message.value)
+            return True
+        if self.isPrimary and message.command != MtMessage.MSG_ACCEPTOR_ACCEPT:
+            self.instances[message.instanceID].getProtocol(message.proposalID).doTransition(message)
+        if message.command == MtMessage.MSG_ACCEPTOR_ACCEPT:
+            if message.instanceID not in self.instances:
+                self.instances(message.instanceID) = MtInstanceRecord()
+            record = self.instances[message.instanceID] 
+            if message.proposalID not in record.cleanProtocols:
+                protocol = MtPaxosLeaderProtocol(self)
+                protocol.state = MtPaxosLeaderProtocol.STATE_AGREED
+                protocol.proposalID = message.proposalID
+                protocol.instanceID = message.instanceID
+                protocol.value = message.value
+                record.addProtocol(protocol)
+                print(u'leader:{} new propose:{} instance:{} acceptor:{} value:{}'.format(self.port, message.proposalID, message.instanceID, message.source, message.value))
+            else:
+                protocol = record.getProtocol(message.proposalID)
+            protocol.doTransition(message)
+        return True
+
+    def newProposal(self, value, instance=None):
+        protocol = MtPaxosLeaderProtocol(self)
+        if instance == None:
+            self.highestInstance += 1
+            instanceID = self.highestInstance
+        else:
+            instanceID = instance
+        self.proposalCount += 1
+        id = (self.port, self.proposalCount)
+        if instanceID in self.instances:
+            record = self.instances[instanceID]
+        else:
+            record = MtInstanceRecord()
+            self.instances[instanceID] = record
+        protocol.propose(value, id, instanceID)
+        record.addProtocol(protocol)
+    
+    def notifyLeader(self, protocol, message)
+        if protocol.state == MtPaxosLeaderProtocol.STATE_ACCEPTED:
+            print(u'leader:{} accepted instance:{} value:{} sender:{}'.format(self.port, message.instanceID, message.value, message.source) )
+            self.instances[message.instanceID].accepted = True
+            self.instnaces[message.instanceID].value = message.value
+            self.highestInstance = max(message.instanceID, self.highestInstance)
+            return
+        if protocol.state == MtPaxosLeaderProtocol.STATE_REJECTED:
+            # try again using next instance
+            self.proposalCount = max(self.proposalCount, message.highestPID[1])
+            self.newProposal(message.value) 
+            return True
+        if protocol.state == MtPaxosLeaderProtocol.STATE_UNACCEPTED:
+            pass
+            
+
+
+
+
+
+    
---
+++ MtPaxosMain.py
@@ -0,0 +1,56 @@
+# coding = utf-8
+import threading,socket,pickle,random
+from multiprocessing import Queue 
+from MtMessagePump import MtMessageBump
+from MtMessage import MtMessage
+from MtInstanceRecord import MtInstanceRecord
+from MtPaxosLeader import MtPaxosLeader
+from MtPaxosLeaderProtocol import MtPaxosLeaderProtocol
+from MtPaxosAcceptorProtocol import MtPaxosAcceptorProtocol
+from MtPaxosAcceptor import MtPaxosAcceptor
+
+import time
+SVR_PORT_ONE = 34321
+SVR_PORT_TWO = 34322
+CLIENT_PORT_ONE = 43420
+
+if __name__ == '__main__':
+    numclients = 5
+    clients = [MtPaxosAcceptor(port, [SVR_PORT_ONE, SVR_PORT_TWO]) for port in range(CLIENT_PORT_ONE, CLIENT_PORT_ONE + numclients)]
+    leader1 = MtPaxosLeader(SVR_PORT_ONE, [SVR_PORT_TWO], [c.port for c in clients])
+    leader2 = MtPaxosLeader(SVR_PORT_TWO, [SVR_PORT_ONE], [c.port for c in clients])
+    
+    # start leaders
+    leader1.start()
+    leader2.setPrimary(True)
+    leader2.start()
+    for c in clients:
+        c.start()
+
+    # simulating clients failure
+    clients[0].fail()
+    clients[1].fail()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    start=time.time()
+    for i in range(30)
+        m = MtMessage(MtMessage.MSG_EXT_PROPOSE)
+        m.value = 0 + i
+        m.to = SVR_PORT_TWO # notify leader
+        mdata = pickle.dumps(m)
+        s.sendto(mdata, ('localhost', m.to))
+
+    print('leader: {} getNumAccepted: {}'.format(leader1.port, leader1.getNumAccepted()))
+    time.sleep(10)
+    leader1.stop()
+    leader2.stop()
+    for c in clients:
+        c.stop()
+    print ("leader: {} history: {}".format(leader1.port, leader1.getHistory()))
+    print ("leader: {} history: {}".format(leader2.port, leader2.getHistory()))
+    end=time.time()
+
+    print('total time:{}'.format(end - start))
+    
+
+
